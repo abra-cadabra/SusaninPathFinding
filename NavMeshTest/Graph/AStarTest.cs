2ca22e8615ddc42a298d86995dccc3d5756aa41c
PanCotzky <pancotzky@gmail.com>
2012-11-11 08:54:26 +0200

    Added AStarTest

---
+++ NavMeshTest/Graph/AStarTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNavMesh.Graph.PathFinding;
+using TDDTests;
+
+namespace NavMeshTest.Graph
+{
+    class AStarTest : TestOf<AStar<Node>>
+    {
+        public override void InitializeSystemUnderTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CleenUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
